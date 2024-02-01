1945400eb6e7cc296ae261f1e99efae9947b2457
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2024-02-01 10:19:47 +0100

    Debugging github CI

--- Sources/libssh2-apple/main.swift
+++ Sources/libssh2-apple/main.swift
@@ -89,6 +89,30 @@ for p in Config.platforms {
     try? mkdir(libPath)
 
     try sh(
+      "echo Command: cmake",
+      "-Hlibssh2 -B\(binPath)",
+      "-GXcode",
+      "-DCMAKE_TOOLCHAIN_FILE=\(toolchain)",
+      "-DCMAKE_C_COMPILER=\(p.ccPath())",
+      "-DCMAKE_OSX_ARCHITECTURES=\(arch)",
+      "-DCMAKE_OSX_DEPLOYMENT_TARGET=\(p.deploymentTarget)",
+      "-DBUILD_SHARED_LIBS=OFF",
+      "-DWITH_EXAMPLES=OFF",
+      "-DCMAKE_BUILD_TYPE=Release",
+      "-DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO",
+      "-DCMAKE_SYSTEM_PROCESSOR=\(arch)",
+//      "-DCMAKE_C_FLAGS=\"-target x86_64-apple-ios13.0-macabi -mios-version-min=13.0 -isystem \(try p.sdkPath())/System/iOSSupport/usr/include -iframework /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/iOSSupport/System/Library/Frameworks\"",
+//      "-DCMAKE_CXX_FLAGS=\"-target x86_64-apple-ios13.0-macabi -mios-version-min=13.0\"",
+//      "-DCMAKE_LDFLAGS=\"-target x86_64-apple-ios13.0-macabi  -L/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/maccatalyst  -L/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/iOSSupport/usr/lib -iframework /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/iOSSupport/System/Library/Frameworks \"",
+      "-DCMAKE_INSTALL_PREFIX=\(libPath)",
+      "-DBUILD_EXAMPLES=NO",
+      "-DBUILD_TESTING=NO",
+      "-DENABLE_ZLIB_COMPRESSION=YES",
+      "-DENABLE_CRYPT_NONE=YES",
+      "-DENABLE_MAC_NONE=YES"
+      , env: env)
+
+    try sh(
       "cmake",
       "-Hlibssh2 -B\(binPath)",
       "-GXcode",
